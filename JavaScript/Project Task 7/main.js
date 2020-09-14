93b0e258b8a74b0112e0e4a3e53b433c3f8c8a77
khareshreya <khareshreya1999@gmail.com>
2020-09-14 23:15:22 +0530

    done 7

---
+++ JavaScript/Project Task 7/main.js
@@ -0,0 +1,16 @@
+
+const myAge= 21
+
+var earlyYears=2
+
+earlyYears*=10.5
+
+var laterYears= myAge-2
+
+laterYears*=4
+
+var myAgeInDogYears= earlyYears+laterYears
+
+var myName= "Shreya".toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
