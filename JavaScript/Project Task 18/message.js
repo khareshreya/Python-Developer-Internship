1d0b0b2d098d40156882dad9c27c4a15ff2b3a9e
khareshreya <khareshreya1999@gmail.com>
2020-09-20 00:28:12 +0530

    done

---
+++ JavaScript/Project Task 18/message.js
@@ -0,0 +1,11 @@
+let mod = require( "./messageMixer.js" );
+function displayMessage() {
+  console.log(mod.countCharacter("What is the color of the sky?", "t"));
+  console.log(mod.capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+  console.log(mod.reverseWord("What is the color of the sky?"));
+  console.log(mod.reverseAllWords("What is the color of the sky?"));
+  console.log(mod.replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+  console.log(mod.encode("What is the color of the sky?"));
+}
+
+displayMessage();
